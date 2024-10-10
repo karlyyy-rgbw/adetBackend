3a5406a1f5e8eb04dc7f5af9f06b3cd046db9354
karlyyy-rgbw <kvsuayan@gbox.ncf.edu.ph>
2024-10-10 14:33:56 +0800

    sixth commit

--- routes/crsRoutes.js
+++ routes/crsRoutes.js
@@ -1,13 +1,20 @@
 const express = require('express');  
-const { getAllCourses, getCourseById, createCourse,updateCourse, deleteCourse } = require('../controllers/crsController');  
+const {   
+    getAllStudents,   
+    getStudentById,   
+    createStudent,   
+    updateStudent,   
+    deleteStudent   
+} = require('../controllers/stdController');  
+
 const authenticateToken = require('../middlewares/authMiddleware');  
 
-const router = express.Router();
+const router = express.Router();  
 
-router.get('/', authenticateToken, getAllCourses);      
-router.get('/:id', authenticateToken, getCourseById);
-router.post('/', authenticateToken, createCourse); 
-router.put('/:id', authenticateToken, updateCourse);    
-router.delete('/:id', authenticateToken, deleteCourse);   
+router.get('/', authenticateToken, getAllStudents);          
+router.get('/:id', authenticateToken, getStudentById);      
+router.post('/', authenticateToken, createStudent);          
+router.put('/:id', authenticateToken, updateStudent);        
+router.delete('/:id', authenticateToken, deleteStudent);     
 
 module.exports = router;
